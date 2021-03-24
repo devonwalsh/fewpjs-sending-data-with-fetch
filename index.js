8e73973573222a7935cf4e1234caccd888452a6f
Devon Walsh <devonwalsh@gmail.com>
2021-03-23 20:40:21 -0400

    commit tree 3267a1c43f04f09ee67c4d2070fab2f2a3c9b3e3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail
+      };
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(json => document.body.innerHTML = json.id)
+        .catch(error => document.body.innerHTML = error.message)
+}
